c949ba5fd6e7a2a745a29d660cd5987f747b76af
unknown <shinichi_jnie@hotmail.com>
2021-08-15 15:56:42 -0700

    adding libs

--- apps/eresturant/src/app/app.module.ts
+++ apps/eresturant/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router, RouterModule, Routes } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -8,6 +9,8 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import {UiModule} from '@jnresturant/ui';
+import {ButtonModule} from 'primeng/button';
+
 
 const router:Routes = [
   {path: '', component: HomePageComponent},
@@ -16,7 +19,7 @@ const router:Routes = [
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(router), UiModule],
+  imports: [BrowserModule,BrowserAnimationsModule, RouterModule.forRoot(router), UiModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
