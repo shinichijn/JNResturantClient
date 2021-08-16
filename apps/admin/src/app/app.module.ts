2f7ee990399c7a4d6518eab3f401656bf7980698
unknown <shinichi_jnie@hotmail.com>
2021-08-15 17:21:31 -0700

    add sidebar

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const  routers: Routes = [
+  {
+    path:'',
+    component: ShellComponent,
+    children: [
+      {
+        path:'dashboard',
+        component:DashboardComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShellComponent, SidebarComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routers, { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jnresturant-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
